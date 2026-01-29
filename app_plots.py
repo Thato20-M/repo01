9d037c5aa45bfed5b1e9325955523c22ad81bce7
Thato Morwatshehla <teteemorwa@gmail.com>
2026-01-29 09:38:47 +0200

    Add files via upload

---
+++ app_plots.py
@@ -0,0 +1,22 @@
+# My Plot of data
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.title("Title heading")
+
+st.write("Hello, Streamlit!")
+
+st.header("Sample Data")
+
+data = pd.DataFrame({"x": [1, 2, 3, 4, 5, 5, 6, 7], "y": [11, 25, 30, 32, 38, 12, 85]})
+
+# Display the data in the Streamlit app
+st.write(data)
+
+# Create a Plotly figure
+fig = px.line(data, x="x", y="y", title="Simple Plotly Example")
+
+# Display the plot in the Streamlit app
+st.plotly_chart(fig)

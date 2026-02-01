a898cbd60259f34818d9c26ce71b6bcd14ba5c43
Thato Morwatshehla <teteemorwa@gmail.com>
2026-02-01 23:13:01 +0200

    Update predictor.py

--- ml/predictor.py
+++ ml/predictor.py
@@ -1,1 +1,14 @@
+import joblib
+import os
+
+class AcademicPredictor:
+    def __init__(self, model_path="models"):
+        self.regressor = joblib.load(os.path.join(model_path, "regressor.pkl"))
+        self.classifier = joblib.load(os.path.join(model_path, "classifier.pkl"))
+
+    def predict(self, features_df):
+        return {
+            "next_mark": float(self.regressor.predict(features_df)[0]),
+            "risk_prob": float(self.classifier.predict_proba(features_df)[0][1])
+        }
 

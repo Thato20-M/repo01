3f330ab74b56f8534326beae72d571eabf8dee9d
Thato Morwatshehla <teteemorwa@gmail.com>
2026-02-02 00:17:00 +0200

    Update myport.py

--- myport.py
+++ myport.py
@@ -116,7 +116,7 @@ page = st.sidebar.radio(
         
     ]
 )
-st.sidebar.title("Dashboard in develpment by Thato ;-)")
+st.sidebar.write("Dashboard in develpment by Thato ;-)")
 # =============================
 # DASHBOARD
 # =============================
